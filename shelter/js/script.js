93b743df1cb67539d85485a2d2219aee96521491
Arseniy <krutoi.arseniy@gmail.com>
2024-08-26 21:27:20 +0300

    feat: correct mistakes in burger-menu

--- shelter/js/script.js
+++ shelter/js/script.js
@@ -1,9 +1,79 @@
-      document.getElementById('header-burger').addEventListener('click', function() {
-         document.querySelector('.header-burger').classList.toggle('open')
-         document.querySelector('.header-nav').classList.toggle('open')
-         document.querySelector('.header').classList.toggle('open')
-         document.querySelector('.header-background').classList.toggle('open')
-         document.querySelector('.body').classList.toggle('open')
+      const headerBurger = document.querySelector('.header-burger');
+      const headerNav =  document.querySelector('.header-nav');
+      const headerBackground =  document.querySelector('.header-background');
+      const body =  document.querySelector('.body');
+      
+     headerBurger.addEventListener('click', function() {
+         headerBurger.classList.toggle('open')
+         headerNav.classList.toggle('open')
+         headerBackground.classList.toggle('open')
+         body.classList.toggle('open')
     })
 
+    headerBackground.addEventListener('click', function() {
+      if (headerNav.classList.contains('open')) {
+         headerNav.classList.remove('open');
+      }
+
+      if (headerBurger.classList.contains('open')) {
+         headerBurger.classList.remove('open');
+      }
+      
+      if (headerBackground.classList.contains('open')) {
+         headerBackground.classList.remove('open');
+      }
+
+      if (body.classList.contains('open')) {
+       body.classList.remove('open');
+      }
+    })
+
+    const headerNavRevers = document.querySelector('.header-nav-revers');
+    const headerNavA = document.querySelectorAll('.header-nav-a');
+
+      function removeClass() {
+        if (headerNav.classList.contains('open'))
+          headerNav.classList.remove('open');
+  
+      if (headerBurger.classList.contains('open'))
+          headerBurger.classList.remove('open');
+  
+      if (headerBackground.classList.contains('open'))
+          headerBackground.classList.remove('open');
+  
+      if (body.classList.contains('open'))
+          body.classList.remove('open');
+     }
     
+      headerNavA.forEach(el => {
+        el.addEventListener('click', removeClass)
+      })
+
+   headerNavRevers.addEventListener('click', function(){
+        if (headerBurger.classList.contains('open')) {
+             headerBurger.classList.remove('open');
+        }
+        if (headerNav.classList.contains('open')) {
+             headerNav.classList.remove('open');
+        }
+
+        if (headerBackground.classList.contains('open')) {
+             headerBackground.classList.remove('open');
+        }
+
+        if (body.classList.contains('open')) {
+             body.classList.remove('open');
+        }
+      })
+      
+     
+      
+
+ 
+
+    
+/* 
+    document.getElementById('jennifer').addEventListener('click', function(){
+   
+    })
+ */
