a578fc116b5532e88527de3988b6347c6abacff9
Arseniy <krutoi.arseniy@gmail.com>
2024-09-18 20:28:44 +0300

    feat: add new pictures and add play audio

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,8 +1,22 @@
-/* const audio = document.querySelector("audio");
-function playAudio() {
-  audio.currentTime = 0;
-  audio.play();
+// const audio = document.querySelector('.audio');
+const playPauseButton = document.querySelector('.play-pause-button');
+let isPlay = false;
+
+const audio = new Audio();
+
+if (isPlay == false) {
+  // let isPlay = true;
+  function playAudio() {
+    audio.src = './audio/Coldplay feat. Beyonce - Hymn For The Weekend.mp3'
+    audio.currrntTime = 0;
+    audio.play();
+  }
+} else if (!isPlay) {
+    // let isPlay= false;
+    function pauseAudio() {
+      audio.pause();
+    }
 }
-function pauseAudio() {
-  audio.pause();
-} */
+  playPauseButton.addEventListener('click', playAudio);
+  playPauseButton.addEventListener('click', pauseAudio);
+  
