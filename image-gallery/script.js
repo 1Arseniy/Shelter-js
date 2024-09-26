f9a1e3e483ce303ac6e9e2d959a7db9dbd017c42
Arseniy <krutoi.arseniy@gmail.com>
2024-09-26 20:54:02 +0300

    feat: add async

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,13 +1,18 @@
 const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const pictures = document.querySelector('.pictures');
 
 function getData() {
-    fetch(url)
+    fetch('https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo')
      .then((res) => res.json())
      .then((data) => {
-        console.log(data.results[0].urls.regular);
+        console.log(data);
     })
-   /*  function showData(data) {
-       console.log(data.results[0].urls.regular);
-   } */
-}
-    getData();
+}  
+ 
+
+/* async function getData() {
+    const data = await fetch(url);
+    const reversData = await data.json();
+    console.log(data);
+} */
+ getData();
