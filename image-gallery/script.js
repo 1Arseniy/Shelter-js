d3fb14ec6b9e718e68b25ce9aa1bc2d47ec3acfc
Arseniy <krutoi.arseniy@gmail.com>
2024-09-27 21:30:45 +0300

    feat: correct mistakws

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,12 +1,21 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const url = 'Rdo9rHMrwWcBnTDIwzUYthfkiVTJ65wg4Rl7hP-AnQs';
 const pictures = document.querySelector('.pictures');
 
 function getData() {
-    fetch('https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo')
+    fetch(`https://api.unsplash.com/search/photos?query=phone&per_page=30&orientation=landscape&client_id=${url}`)
      .then((res) => res.json())
      .then((data) => {
+       /*  data.forEach((photo, index) => {
+            console.log(photo)
+        }) */
         console.log(data);
-    })
+           pictures.src = data.results[0].urls.regular;
+        //    pictures[index].parentElement.appendChild()
+        console.log(pictures)
+    }) .catch ((err) => {
+       console.log(err);
+      }
+    )
 }  
  
 
