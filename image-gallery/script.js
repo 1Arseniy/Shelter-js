7c9262461cc975a58beb787bc70ffe98746307e6
Arseniy <krutoi.arseniy@gmail.com>
2024-09-28 21:52:49 +0300

    feat: add unplash-logo

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,27 +1,21 @@
-const url = 'Rdo9rHMrwWcBnTDIwzUYthfkiVTJ65wg4Rl7hP-AnQs';
-const pictures = document.querySelector('.pictures');
+const url = "Rdo9rHMrwWcBnTDIwzUYthfkiVTJ65wg4Rl7hP-AnQs";
+const pictures = document.querySelector(".pictures");
 
-function getData() {
-    fetch(`https://api.unsplash.com/search/photos?query=phone&per_page=30&orientation=landscape&client_id=${url}`)
-     .then((res) => res.json())
-     .then((data) => {
-       /*  data.forEach((photo, index) => {
-            console.log(photo)
-        }) */
-        console.log(data);
-           pictures.src = data.results[0].urls.regular;
-        //    pictures[index].parentElement.appendChild()
-        console.log(pictures)
-    }) .catch ((err) => {
-       console.log(err);
-      }
+function getData(index) {
+    fetch(
+        `https://api.unsplash.com/search/photos?extras=url_s&query=spring&per_page=30&orientation=landscape&client_id=${url}`
     )
-}  
- 
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            let img = document.querySelector(`.img-${index}`);
+            img.src = data.results[index].urls.regular;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
 
-/* async function getData() {
-    const data = await fetch(url);
-    const reversData = await data.json();
-    console.log(data);
-} */
- getData();
+for (let i = 0; i < 6; i++) {
+    getData(i);
+}
