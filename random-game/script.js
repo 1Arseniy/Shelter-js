6f817b79fa9609b1864ebe5fc07ee423d78327f2
Arseniy <krutoi.arseniy@gmail.com>
2024-10-02 20:42:02 +0300

    refactor: script.js

--- random-game/script.js
+++ random-game/script.js
@@ -3,6 +3,12 @@ const game = canvas.getContext('2d');
 
 let area =  32;
 let count = 0;
+let score = 0;
 
-const foods = new Image() 
+/* const foods = new Image() 
 foods.src = "icon/icon-avacado.png"
+ */
+/* let avacado = {
+    x: Math.floor(Math.random(1 * 17 -1)) * area,
+    y: Math.floor(Math.random(1 * 17 -1)) * area
+} */
