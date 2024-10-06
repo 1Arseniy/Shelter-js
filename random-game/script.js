9404d060eeaff2f6806e9f14fe9967d33ff7f655
Arseniy <krutoi.arseniy@gmail.com>
2024-10-06 22:24:15 +0300

    feat: add footer

--- random-game/script.js
+++ random-game/script.js
@@ -1,3 +1,19 @@
 const cell = document.querySelectorAll('.cell'); 
-let steps = 0;
+let step = 0;
 
+const icons = [
+   './icon/circle.png',
+   './icon/cross.png'
+]
+
+cell.forEach((el) => {
+    el.addEventListinier('click', () => {
+        icons.length[0];
+    })
+})
+
+/* function tap() {
+    cell.src = icons[0]
+}
+
+cell.addEventListinier('click', tap) */
