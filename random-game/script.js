ca2d4f65bedf8c718778c34abdb2cdbdfb7f0ee2
Arseniy <krutoi.arseniy@gmail.com>
2024-10-08 22:18:17 +0300

    feat: add dataset

--- random-game/script.js
+++ random-game/script.js
@@ -1,17 +1,23 @@
 const cell = document.querySelectorAll('.cell'); 
-const png = document.querySelector('img');
+let png = document.querySelectorAll('img');
 let step = 0;
 
 const icons = [
     './icon/circle.png',
     './icon/cross-1.png'
 ];
-
+/*  png.dataset.indexTwo;
+console.log(png.dataset.indexTwo); */
 cell.forEach((el) => {
-    el.addEventListener('click', function() {
-           if(cell !== null) {
-              png.src = icons[1]
-           }
-         
+    el.addEventListener('click', () => {
+       const picture = png[4]
+       png[4].src = icons[0]
+       console.log(picture.dataset);
+
+        // picture.src = './icon/circle.png'
+       /*  png.dataset.indexTwo = '';
+        console.log(png.dataset.indexTwo) */
+      /*   let pngOne = document.querySelector('.png-1')
+        pngOne.src = icons[0]; */
     })
 })
