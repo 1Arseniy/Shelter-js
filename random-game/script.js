52f2880a5215e3576b76b644e02e23e9d3c09d9d
Arseniy <krutoi.arseniy@gmail.com>
2024-10-09 21:16:47 +0300

    fix: correct mistakes

--- random-game/script.js
+++ random-game/script.js
@@ -1,23 +1,15 @@
 const cell = document.querySelectorAll('.cell'); 
-let png = document.querySelectorAll('img');
-let step = 0;
+// let png = document.querySelectorAll('.png');
 
 const icons = [
     './icon/circle.png',
     './icon/cross-1.png'
 ];
-/*  png.dataset.indexTwo;
-console.log(png.dataset.indexTwo); */
+
 cell.forEach((el) => {
     el.addEventListener('click', () => {
-       const picture = png[4]
-       png[4].src = icons[0]
-       console.log(picture.dataset);
-
-        // picture.src = './icon/circle.png'
-       /*  png.dataset.indexTwo = '';
-        console.log(png.dataset.indexTwo) */
-      /*   let pngOne = document.querySelector('.png-1')
-        pngOne.src = icons[0]; */
+        let picture = el.dataset.png
+        let pictureDown = document.querySelector(`.${picture}`)
+        pictureDown.src = icons[0]
     })
 })
