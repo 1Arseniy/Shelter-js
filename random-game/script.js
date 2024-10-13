2cc529bde15fc5cc5f74b5aad6f3d41ed1481d62
Arseniy <krutoi.arseniy@gmail.com>
2024-10-13 21:07:26 +0300

    feat: add console.js

--- random-game/script.js
+++ random-game/script.js
@@ -26,7 +26,7 @@ function addCircle() {
     elemCircle.src = icons[0];
     const parentElemCircle = elemCircle.parentNode;
     parentElemCircle.classList.add("active");
-    // parentElemCircle.classList.add("circle");
+    parentElemCircle.classList.add("circle");
 }
 
 function addCross(el) {
@@ -34,19 +34,27 @@ function addCross(el) {
     let pictureDown = document.querySelector(`.${picture}`);
     pictureDown.src = icons[1];
     el.classList.add("active");
-    // el.classList.add("cross");
+    el.classList.add("cross");
     elemFree = [];
 
     addCircle();
 }
 
-// const finishWidow = document.querySelector("finish-window");
+const finishWidow = document.querySelector("finish-window");
+const png = document.querySelectorAll('.png')
 
 function validationVictory() {
     for (let i = 0; i < win.winConbenation.length; i++) {
         const el0 = document.querySelector(`.cell-${win.winConbenation[0]}`);
-        const el2 = document.querySelector(`.cell-${win.winConbenation[1]}`);
-        const el3 = document.querySelector(`.cell-${win.winConbenation[2]}`);
+        const el1 = document.querySelector(`.cell-${win.winConbenation[1]}`);
+        const el2 = document.querySelector(`.cell-${win.winConbenation[2]}`);
+        
+        if(el0.classList.contains('cross') && el1.classList.contains('cross') && el2.classList.contains('cross') ||  el0.classList.contains('circle') && el1.classList.contains('circle') && el2.classList.contains('circle')) {
+            png.el0.classList.add('filter');
+            png.el1.classList.add('filter');
+            png.el2.classList.add('filter');
+            break
+        }
     }
 }
 
@@ -63,7 +71,7 @@ function showPictures() {
     cell.forEach((el) => {
         el.addEventListener("click", validationFreeCell);
     });
+    
+    validationVictory();
 }
-
-validationVictory();
 showPictures();
